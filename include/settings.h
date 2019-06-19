288b2edb31e73c872ddebc55665e9ff79f63d24b
Lukas Derner Grüdtner <lukasgrudtner@hotmail.com>
2019-06-19 09:46:29 -0300

    Release version.

--- include/settings.h
+++ include/settings.h
@@ -11,9 +11,9 @@
 #define DEFAULT_PORT 1234   /* Porta padrão */
 #define DEFAULT_THREAD_POOL_SIZE 2  /* Tamanho padrão do pool de threads */
 
-#define NUMBER_OF_ACCEPTORS 4   /* Número de acceptors no sistema */
+#define NUMBER_OF_ACCEPTORS 2   /* Número de acceptors no sistema */
 #define NUMBER_OF_PROPOSERS 2   /* Número de proposers no sistema */
-#define NUMBER_OF_LEARNERS 3    /* Número de learners no sistema */
+#define NUMBER_OF_LEARNERS 2    /* Número de learners no sistema */
 
 #define SEPARATOR ";"       /* Separador de informações nas mensagens enviadas */
 #define END_OF_MESSAGE "\n" /* Símbolo de fim de mensagem */
